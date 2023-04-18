fc997e8d94e6a9649a29457909bf282809e0317b
sayem mahmud <sayem.mahmud97@gmail.com>
2023-04-18 19:35:33 +0600

    done

--- src/main.ts
+++ src/main.ts
@@ -9,147 +9,123 @@ const metaTags:any = [];
 
 require('console-stamp')(console, '[HH:MM:ss.l]');
 const main = async () => {
-  let arr = []
   try {
-    // File path to the text file
-    const filePath = 'dataurl.txt';
-
+    //get browser instance
+    const browser = await chromium.launch({ headless: false });
   
-    // Read the contents of the text file synchronously
-    const data = fs.readFileSync(filePath, 'utf8');
-  
-    // File contents are stored in 'data'
-    console.log('data', data);
-    arr = data.trim().split('\r\n')
-    console.log('aaaaa', arr);
-  } catch (err) {
-    // Handle error if any
-    console.error(`Failed to read file: ${err}`);
-  }
+    // Create a new browser context
+  const context = await browser.newContext();
 
-  const whatAlready = async () => {
-    let already = []
-    const filePath2 = 'metaEntry.txt';
-    const data2 = await fs.readFileSync(filePath2, 'utf8');
-    console.log('data2', data2);
-    already = data2.trim().split('\n')
-    console.log('aaaaa2', already);
-    return already
-  }
-
-
-  //get browser instance
-  const browser = await chromium.launch({ headless: false });
-  
-  const page = await browser.newPage();
-  for (const url of arr) {
-    const alreadyData = await whatAlready()
-    console.log('alreadyData', alreadyData)
-    // Navigate to the current URL
-    console.log('url', url)
-
-    const has = alreadyData.includes(url);
-    try {
-      // if (db.has(url) == true) {
-      // continue
-      // }
-      console.log('hass', has)
-      if (has == true) {
-        continue
-      }
-
-
-      await page.goto(url);
-
-      const extractedMetaTags = await page.evaluate(() => {
-        const metaTags: Array<{ name: any, content: any }> = [];
-        const metaTagElements = document.querySelectorAll('meta');
+  // Create a new page
+  const page = await context.newPage();
+    // const page = await browser.newPage();
+ 
+    for (var i = 1; i < 3; i++) {
+      await page.goto(`https://www.revdl.com/page/${i}/`);
+      const allReadMoreHref = await page.evaluate(() => {
+        const metaTags = [];
+        const metaTagElements = document.querySelectorAll('.tpcrn-read-more');
+        console.log('metaTagElements',metaTagElements);
         metaTagElements.forEach(metaTagElement => {
-          const metaTag: any = {};
-          metaTag.name = metaTagElement.getAttribute('name') || metaTagElement.getAttribute('property') || '';
-          metaTag.content = metaTagElement.getAttribute('content') || '';
-          metaTags.push(metaTag);
+          const href = metaTagElement.getAttribute('href') || '';
+          console.log('href',href)
+          metaTags.push(href);
         });
         return metaTags;
       });
 
-      // await browser.close();
+      for (var j = 0; j < allReadMoreHref.length; j++){
+        var apkObj: any = {}
+        
+
+        await page.goto(allReadMoreHref[j])
+  
+        const title = await page.locator('.post-title h1').innerText() || ''
+        const imgSrc = await page.locator('.attachment-featured_image').getAttribute('src') ||''
+        const createdAt = await page.locator('.post-date').innerText() || ''
+
+        const categoriesInnerText = await page.$eval('.entry_categories', (element) => {
+          const anchors = Array.from(element.querySelectorAll('a'));
+          const innerTextArray = anchors.map((anchor) => anchor.innerText ||'');
+          const concatenatedText = innerTextArray.join(',');
+          console.log('concatenatedText',concatenatedText)
+          return concatenatedText;
+        });
+        
+        const fileVersionsSizeDeveloper = await page.$$eval('.dl-size', (elements) => {
+          return elements.map(element => {
+            const secondSpan = element.querySelector('span:nth-child(2)');
+            return secondSpan ? secondSpan.textContent.trim() : '';
+          });
+        });
       
-      metaTags.push({ url, metaTags: extractedMetaTags });
-      console.log('metaTags', metaTags);
-      console.log('url', url)
-      // db.set(url, 'value');
-      // fs.writeFileSync('metaEntry.txt', url);
-      fs.appendFileSync('metaEntry.txt', url + '\n' , 'utf8');
+        const version = fileVersionsSizeDeveloper[0];
+        const fileSize = fileVersionsSizeDeveloper[1];
+        const developer = fileVersionsSizeDeveloper[2];
+
+        const parentInnerText = await page.$eval('.post_content.entry-content', (element) => {
+          // Get all the child elements under the parent element
+          const children = Array.from(element.children);
+          // Filter out child elements with class "wp-caption" or "hatom-extra"
+          const filteredChildren = children.filter((child) => !child.classList.contains('wp-caption') && !child.classList.contains('hatom-extra'));
+          // Extract the innerText from the filtered child elements
+          const innerTextArray = filteredChildren.map((child) => child.textContent.trim());
+          // Join the innerText values with commas
+          return innerTextArray.join(', ');
+        });
+      
+        console.log('innerTexts', parentInnerText); // Output the innerTexts to the console
+        
+      // // megamind
+      //  const imgSrcs = await page.$$eval('.post_content.entry-content img', imgs => imgs.map(img => img.getAttribute('src')));
+
+      // // Concatenate the img srcs with commas
+      // const imgSrcsString = imgSrcs.join(',');
+
+      // console.log(imgSrcsString);
+   
+        const downloadButtons = await page.$$('.download_button');
+        // Click on the first download button
+        if (downloadButtons.length > 0) {
+          await downloadButtons[0].click();
+          // Wait for navigation to complete with an increased timeout value of 30 seconds
+          await page.waitForTimeout(2000); // Add a delay to allow time for new tab to open
     
-    }
-    catch (error) {
-      console.log('what');
-      // fs.writeFileSync('metaEntry.txt', url);
-      fs.appendFileSync('metaEntry.txt', url + '\n', 'utf8');
-      fs.appendFileSync('metaEntryNot.txt', url + '\n' , 'utf8');
-      // db.set(url, 'value');
-      continue
+          // Get the newly opened page
+          const pages = await context.pages();
+          const newPage = pages[pages.length - 1];
+      
+          const newPageExtractedMetaTags = await newPage.evaluate(() => {
+            const metaTagsArr: any = [];
+            const metaTagElements = document.querySelectorAll('.dl a');
+            metaTagElements.forEach(metaTagElement => {
+              const metaTag: any = {};
+              metaTag.href = metaTagElement.getAttribute('href') || '';
+              metaTag.innerText = metaTagElement.textContent.trim() || '';
+              metaTagsArr.push(metaTag);
+            });
+            return metaTagsArr;
+          });
+        apkObj.title = title
+        apkObj.imgSrc = imgSrc
+        apkObj.createdAt = createdAt
+        apkObj.categories= categoriesInnerText
+        apkObj.version= version
+        apkObj.fileSize= fileSize
+        apkObj.developer= developer
+        apkObj.allText = parentInnerText
+        apkObj.downloadFile =newPageExtractedMetaTags
+        metaTags.push(apkObj)
+        }
+        page.waitForTimeout(5000)
+      }  
     }
   }
-  fs.writeFileSync('metaTags.json', JSON.stringify(metaTags, null, 2));
-  console.log('Meta tags saved to metaTags.json');
-  await page.close();
+  
+  catch (error) {
+   console.log('eeee', error)
+    }
+   fs.writeFileSync('metaTags.json', JSON.stringify(metaTags, null, 2));
 }
 
 main()
-
-
-
-// import { chromium } from "playwright";
-// const fs = require('fs');
-// const main = async () => {
-  
-//   //get browser instance
-//   const browser = await chromium.launch({ headless: false, timeout:1000*60*10});
-  
-//   const page = await browser.newPage();
-//   await page.goto('https://www.google.com');
-//   await page.locator('.gLFyf').fill('photo');
-//   await page.keyboard.press('Enter');
-//  // Wait for navigation to complete
-//  await Promise.all([
-//   page.waitForNavigation(),
-//   page.waitForLoadState(), // Add this line to ensure page load state is complete
-// ]);
-//         const extractedMetaTags = await page.evaluate(() => {
-//             const metaTags = [];
-//             const metaTagElements = document.querySelectorAll('.yuRUbf > a');
-//           metaTagElements.forEach( metaTagElement => {
-//             console.log('metaTagElement', metaTagElement)
-//             const href = metaTagElement.getAttribute('href')
-//             console.log('href',href)
-//             metaTags.push(href)
-//             //   metaTag.name = metaTagElement.getAttribute('name') || metaTagElement.getAttribute('property') || '';
-//             //   metaTag.content = metaTagElement.getAttribute('content') || '';
-//             //   metaTags.push(metaTag);
-//             // });
-            
-//           })
-//          return metaTags
-//         });
-//  console.log('extractedMetaTags',extractedMetaTags);
-//   for (var i = 0; i < extractedMetaTags.length; i++){
-  
-//     await page.goto('https://www.similarweb.com')
-//     await page.waitForTimeout(3000)
-//     await page.locator('.app-search__input').fill(extractedMetaTags[i]);
-//     await page.keyboard.press('Enter'); 
-//     await Promise.all([
-//       page.waitForNavigation(),
-//       page.waitForLoadState(), // Add this line to ensure page load state is complete
-//     ]);
-//     await page.waitForTimeout(10000)
-//     const globe = document.querySelectorAll('wa-rank-list__value')
-//     var pValue = globe[0].innerHTML;
-//     var extractedValue = pValue.split('<small>#</small>')[1];
-//     fs.appendFileSync('hello.txt', extractedMetaTags[i] +'-' + extractedValue + '\n' , 'utf8');
-//   }
-// }
-
-// main(  )
